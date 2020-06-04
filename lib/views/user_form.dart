5d2a41a5de2e000065b7268e7fffd7f3239efc66
Thalisson Silva <54037942+thalissonms@users.noreply.github.com>
2020-06-04 05:22:43 -0300

    New
    Flutter User Register/Edit/Remove

---
+++ lib/views/user_form.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter_crud/provider/users.dart';
+import 'package:flutter_crud/models/user.dart';
+
+class UserForm extends StatelessWidget {
+  final _form = GlobalKey<FormState>();
+  final Map<String, String> _formData = {};
+
+  void _loadFormData(User user){
+    if(user != null){
+      _formData['id'] = user.id;
+      _formData['name'] = user.name;
+      _formData['email'] = user.email;
+      _formData['avatarUrl'] = user.avatarUrl;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final User user = ModalRoute.of(context).settings.arguments;
+    
+    _loadFormData(user);
+    print(_formData['id']);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Formulário de cadastro'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.save),
+            onPressed: () {
+              final isValid = _form.currentState.validate();
+
+              if (isValid) {
+                _form.currentState.save();
+                Provider.of<Users>(context, listen: false).put(User(
+                  id: _formData['id'],
+                  name: _formData['name'],
+                  email: _formData['email'],
+                  avatarUrl: _formData['avatarUrl'],
+                ));
+                Navigator.of(context).pop();
+              }
+            },
+          )
+        ],
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(15),
+        child: Form(
+            key: _form,
+            child: Column(children: <Widget>[
+              TextFormField(
+                initialValue: _formData['name'],
+                decoration: InputDecoration(labelText: 'Nome'),
+                validator: (value) {
+                  if (value.trim().isEmpty || value == null) {
+                    return 'Nome de Usuário invalido!';
+                  }
+                  return null;
+                },
+                onSaved: (value) => _formData['name'] = value,
+              ),
+              TextFormField(
+                initialValue: _formData['email'],
+                decoration: InputDecoration(labelText: 'Email'),
+                onSaved: (value) => _formData['email'] = value,
+              ),
+              TextFormField(
+                initialValue: _formData['avatarUrl'],
+                decoration: InputDecoration(labelText: 'URL do Avatar'),
+                onSaved: (value) => _formData['avatarUrl'] = value,
+              )
+            ])),
+      ),
+    );
+  }
+}
